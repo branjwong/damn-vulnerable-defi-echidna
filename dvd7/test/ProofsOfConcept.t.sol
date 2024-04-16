2a004ca22159b696673c5e4da5884a0f9bc4dbcb
Brandon Wong <branjwong@hey.com>
2024-04-16 21:02:37 +0000

    dvd7 add happy path

--- dvd7/test/ProofsOfConcept.t.sol
+++ dvd7/test/ProofsOfConcept.t.sol
@@ -4,6 +4,8 @@ pragma solidity ^0.8.0;
 
 import {Test, console} from "forge-std/Test.sol";
 
+import "@openzeppelin/contracts/token/ERC721/IERC721Receiver.sol";
+
 import "../src/DamnValuableNFT.sol";
 import "../src/Exchange.sol";
 import "../src/TrustfulOracle.sol";
@@ -13,19 +15,20 @@ contract ProofsOfConcept is Test {
     address _attacker = makeAddr("attacker");
 
     Deployer _deployer;
-
-    // DamnValuableTokenSnapshot private _token;
-    // SimpleGovernance private _gov;
-    // SelfiePool private _pool;
+    Exchange _exchange;
+    DamnValuableNFT _nft;
+    TrustfulOracle _oracle;
 
     constructor() {
         _deployer = new Deployer();
-        // (_token, _gov, _pool) = _deployer.deploy();
+        (_exchange, _nft, _oracle) = _deployer.deploy();
     }
 
     function testHappyPath() external {
-        // LoanUser loanUser = new LoanUser(_token, _pool);
-        // loanUser.act();
+        LoanUser loanUser = new LoanUser(_exchange);
+        vm.deal(address(loanUser), 1000 ether);
+
+        loanUser.act();
     }
 
     function testAttack() external {
@@ -41,54 +44,86 @@ contract ProofsOfConcept is Test {
 }
 
 contract Deployer {
-    // uint256 public immutable INITIAL_SUPPLY = 2000000;
-    // uint256 public immutable TOKENS_IN_POOL = 1500000;
-    // function deploy()
-    //     external
-    //     returns (
-    //         DamnValuableTokenSnapshot token,
-    //         SimpleGovernance gov,
-    //         SelfiePool pool
-    //     )
-    // {
-    //     token = new DamnValuableTokenSnapshot(INITIAL_SUPPLY);
-    //     gov = new SimpleGovernance(address(token));
-    //     pool = new SelfiePool(address(token), address(gov));
-    //     token.transfer(address(pool), TOKENS_IN_POOL);
-    //     token.snapshot();
-    // }
+    uint256 public immutable EXCHANGE_INITAL_ETH_BALANCE = 9990 ether;
+    uint256 public immutable INITIAL_NFT_PRICE = 999 ether;
+    string public NFT_SYMBOL = "DVNFT";
+
+    address[] _sources = [
+        0xA73209FB1a42495120166736362A1DfA9F95A105,
+        0xe92401A4d3af5E446d93D11EEc806b1462b39D15,
+        0x81A5D6E50C214044bE44cA0CB057fe119097850c
+    ];
+    string[] _symbols;
+    uint256[] _prices;
+
+    function deploy()
+        external
+        returns (Exchange exchange, DamnValuableNFT nft, TrustfulOracle oracle)
+    {
+        _symbols = [NFT_SYMBOL, NFT_SYMBOL, NFT_SYMBOL];
+        _prices = [INITIAL_NFT_PRICE, INITIAL_NFT_PRICE, INITIAL_NFT_PRICE];
+
+        TrustfulOracleInitializer initializer = new TrustfulOracleInitializer(
+            _sources,
+            _symbols,
+            _prices
+        );
+        oracle = initializer.oracle();
+        exchange = new Exchange(address(oracle));
+        nft = exchange.token();
+    }
 }
 
-contract LoanUser {
-    // ERC20Snapshot private _token;
-    // SelfiePool private _pool;
-    // constructor(ERC20Snapshot token, SelfiePool pool) {
-    //     _token = token;
-    //     _pool = pool;
-    // }
+contract LoanUser is IERC721Receiver {
+    Exchange _exchange;
+
+    constructor(Exchange exchange) {
+        _exchange = exchange;
+    }
+
+    function onERC721Received(
+        address /* operator */,
+        address /* from */,
+        uint256 /* tokenId */,
+        bytes calldata /* data */
+    ) external pure returns (bytes4) {
+        return this.onERC721Received.selector;
+    }
+
     function act() external {
         // log ETH for user and exchange
+        console.log("User ETH: %d", address(this).balance);
+        console.log("Exchange ETH: %d", address(_exchange).balance);
+
         // log price of NFT
+        uint256 initialPrice = _exchange.oracle().getMedianPrice("DVNFT");
+        console.log("NFT price: %d", initialPrice);
+
         // buy NFT
+        uint256 tokenId = _exchange.buyOne{value: initialPrice + 1 ether}();
+
         // log ETH for user and exchange
+        console.log("User ETH: %d", address(this).balance);
+        console.log("Exchange ETH: %d", address(_exchange).balance);
+
         // log price of NFT
+        uint256 postBuyPrice = _exchange.oracle().getMedianPrice("DVNFT");
+        console.log("NFT price: %d", postBuyPrice);
+
         // sell NFT
+        _exchange.token().approve(address(_exchange), tokenId);
+        _exchange.sellOne(tokenId);
+
         // log ETH for user and exchange
+        console.log("User ETH: %d", address(this).balance);
+        console.log("Exchange ETH: %d", address(_exchange).balance);
+
         // log price of NFT
+        uint256 postSellPrice = _exchange.oracle().getMedianPrice("DVNFT");
+        console.log("NFT price: %d", postSellPrice);
     }
-    // function receiveTokens(address token, uint256 amount) external {
-    //     console.log("Received flash loan: %d", amount);
-    //     console.log(
-    //         "Current balance: %d",
-    //         ERC20Snapshot(token).balanceOf(address(this))
-    //     );
-    //     ERC20Snapshot(token).transfer(address(_pool), amount);
-    //     console.log("Returned loan of %d", amount);
-    //     console.log(
-    //         "Current balance: %d",
-    //         ERC20Snapshot(token).balanceOf(address(this))
-    //     );
-    // }
+
+    receive() external payable {}
 }
 
 contract Attack is Test {
